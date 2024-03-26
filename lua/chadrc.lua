379d2694e356ae720f2ae164ce4d8c518df02345
yzzeyu <triankyy@icloud.com>
2024-03-26 14:41:41 +0700

    init nvim config file

---
+++ lua/chadrc.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.ui = { theme = "doomchad" }
+
+return M
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "typescript-language-server",
+      "tailwindcss-language-server",
+      "eslint-lsp",
+      "lua-language-server",
+      "html-lsp",
+      "prettier",
+      "stylua"
+    }
+  }
+}
