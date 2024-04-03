ca529d01204ca1be300a31ab0828a89a2a714d14
Trian Oky <110795922+triankyy@users.noreply.github.com>
2024-04-03 21:53:43 +0700

    Update chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,5 +1,17 @@
 local M = {}
 
-M.ui = { theme = "doomchad" }
+M.ui = { 
+  theme = "doomchad",
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "default"
+  },
+  tabufline = {
+    order = { "treeOffset", "buffers", "tabs" }
+  },
+  lsp = {
+    signature = false,
+  }
+}
 
 return M
